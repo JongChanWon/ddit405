a33e5e6618f46936675d7fdc788493d57f711dde
PC-25 <PC-25@DESKTOP-KDJD5VK>
2023-06-02 18:30:56 +0900

    dataType

--- chapter01/src/chap01/sec01/HelloWorld.java
+++ chapter01/src/chap01/sec01/HelloWorld.java
@@ -1,8 +1,22 @@
 package chap01.sec01;
 
+import java.util.Random;
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
+		Random rm = new Random();
+		int a = rm.nextInt();
+		System.out.println(a);
+		int age = 33;
 
+		if (a > age) {
+			System.out.println("내나이가 어때서");
+		} else {
+			System.out.println("니나이가 어때서");
+		}
+
+		String st = new String("dd");
+		System.out.println(st);
 	}
 }
